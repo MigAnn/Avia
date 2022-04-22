87da1151b2e8e1847c716a43ff8c244a01eede35
Анна <anakatkova166@yandex.ru>
2022-04-22 21:10:56 +0300

    New

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 100;
+        int amount = 1;
+
+     int bonus = ( cost * amount );
+        System.out.println("Вам начисленно : " + bonus + " милей.");
+        }
+
+    }
